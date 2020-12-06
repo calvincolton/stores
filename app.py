6b211cdcc38edc422a5af47637d35b317d2fe4d2
Calvin Colton <calvincolton@gmail.com>
2020-12-06 15:22:08 -0800

    github oAuth

--- app.py
+++ app.py
@@ -7,18 +7,21 @@ from flask_uploads import configure_uploads, patch_request_class
 from marshmallow import ValidationError
 from flask_migrate import Migrate
 
+load_dotenv(".env", verbose=True)
+
 from db import db
 from ma import ma
+from oa import oauth
 from blacklist import BLACKLIST
 from resources.user import UserRegister, UserLogin, User, TokenRefresh, UserLogout
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 from resources.confirmation import Confirmation, ConfirmationByUser
+from resources.github_login import GithubLogin, GithubAuthorize
 from resources.image import ImageUpload, Image, AvatarUpload, Avatar
 from libs.image_helper import IMAGE_SET
 
 app = Flask(__name__)
-load_dotenv(".env", verbose=True)
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["PROPAGATE_EXCEPTIONS"] = True
 app.config.from_object("default_config")
@@ -64,6 +67,8 @@ api.add_resource(TokenRefresh, "/refresh")
 api.add_resource(UserLogout, "/logout")
 api.add_resource(Confirmation, "/user_confirm/<string:confirmation_id>")
 api.add_resource(ConfirmationByUser, "/confirmation/user/<int:user_id>")
+api.add_resource(GithubLogin, "/login/github")
+api.add_resource(GithubAuthorize, "/login/github/authorized")
 api.add_resource(ImageUpload, "/upload/image")
 api.add_resource(Image, "/image/<string:filename>")
 api.add_resource(AvatarUpload, "/upload/avatar")
@@ -72,4 +77,5 @@ api.add_resource(Avatar, "/avatar/<int:user_id>")
 if __name__ == "__main__":
     # db.init_app(app)
     ma.init_app(app)
+    oauth.init_app(app)
     app.run(port=5000, debug=True)
